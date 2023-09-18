7c49c1138dbdb307739ad5300c89124f390f16a2
nhacne <142861451+nhacne@users.noreply.github.com>
2023-09-18 10:08:59 +0700

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void menu_highlandcoffee();
+////////////
+int main () {
+	menu_highlandcoffee ();
+}
+void menu_highlandcoffee () {
+	printf("\n\n <3 Highland Coffee <3");
+	printf("\n <> MENU ");
+	printf("\n 1.Cookies & Cream");
+	printf("\n 2.Phindi kem sua");
+	printf("\n 3.Phindi choco");
+	printf("\n 4.Bac xiu da");
+	printf("\n 5.Phin sua da");
+	printf("\n 6.Phin den da");
+}
